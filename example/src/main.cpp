150287f6585f8fdf2f7c64447cc239af7dbd78d2
Lukas Waymann <meribold@gmail.com>
2017-10-31 17:38:01 +0100

    Use a Boost library in the example project

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
 
 #include "strings.hpp"
 
 int main(int argc, char* argv[]) {
    std::cout << hello_world << "\n";
+   std::cout << boost::filesystem::file_size(argv[0]) << "\n";
 }
